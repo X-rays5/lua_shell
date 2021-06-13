756293958c5515f96a24418c2b47de563d701bb7
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-13 18:03:38 +0200

    now the temp file should always be removed

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 #include <string>
 #include <fstream>
 #include <filesystem>
+#include <csignal>
 #include "lua.hpp"
 
+template<class ...Args>
+void DeleteTempfile(Args... args) {
+    if (std::filesystem::exists("lua_shell.tmp"))
+        std::filesystem::remove("lua_shell.tmp");
+}
 void go(lua::lua_state& lua_) {
     std::cout << "lua> ";
     std::string command;
     std::getline(std::cin, command);
-
 
     if (command == "new_lua") {
         lua_ = std::move(lua::lua_state());
@@ -19,18 +24,23 @@ void go(lua::lua_state& lua_) {
                 writer << command;
                 writer.close();
                 lua_.ExcecuteFile("lua_shell.tmp");
-                std::filesystem::remove("lua_shell.tmp");
             } else {
                 std::cout << "Couldn't execute";
             }
         } catch (lua::lua_exception &e) {
             std::cout << e.what() << "\n";
         }
+        DeleteTempfile();
     }
     go(lua_);
 }
 
 int main() {
+    atexit(DeleteTempfile);
+    at_quick_exit(DeleteTempfile);
+    signal(SIGSEGV, DeleteTempfile);
+    std::set_terminate(&DeleteTempfile);
+
     std::cout << "Lua interactive shell started.\n\n";
 
     lua::lua_state lua_;
