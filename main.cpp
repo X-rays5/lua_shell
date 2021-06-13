61a93f8617dc751166f86d7507561d37b981addf
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2021-06-13 18:04:07 +0200

    can now run a multi line scripts so you can use make functions

--- main.cpp
+++ main.cpp
@@ -10,6 +10,35 @@ void DeleteTempfile(Args... args) {
     if (std::filesystem::exists("lua_shell.tmp"))
         std::filesystem::remove("lua_shell.tmp");
 }
+
+void multiline(lua::lua_state& lua_) {
+    std::vector<std::string> multi_line;
+
+    std::string buffer;
+    newline:
+    std::getline(std::cin, buffer);
+    if (buffer != "end_multi_line") {
+        multi_line.emplace_back(buffer);
+        goto newline;
+    } else {
+        try {
+            std::ofstream writer("lua_shell.tmp");
+            if (writer.is_open()) {
+                for (auto&& line : multi_line)
+                    writer << line << "\n";
+                writer.close();
+                lua_.ExcecuteFile("lua_shell.tmp");
+                std::filesystem::remove("lua_shell.tmp");
+            } else {
+                std::cout << "Couldn't execute";
+            }
+        } catch (lua::lua_exception &e) {
+            std::cout << e.what() << "\n";
+        }
+        DeleteTempfile();
+    }
+}
+
 void go(lua::lua_state& lua_) {
     std::cout << "lua> ";
     std::string command;
@@ -17,6 +46,8 @@ void go(lua::lua_state& lua_) {
 
     if (command == "new_lua") {
         lua_ = std::move(lua::lua_state());
+    } else if (command == "new_multi_line") {
+        multiline(lua_);
     } else {
         try {
             std::ofstream writer("lua_shell.tmp");
